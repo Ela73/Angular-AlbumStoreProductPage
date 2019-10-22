d7cb4f1cca1c0cf80e532ccd6fd103f2e7e0ca67
mikael <mikael.johansson@regent.se>
2019-10-22 11:09:49 +0200

    Completed module

--- src/app/product-page/product-page.component.ts
+++ src/app/product-page/product-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductTracklistingComponent } from '../product-tracklisting/product-tracklisting.component'
 
 @Component({
   selector: 'app-product-page',
